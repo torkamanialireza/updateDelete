2bc39351b5523316891ea6b77ebd34a5d357c162
AliReza <torkamani.alireza@gmail.com>
2018-06-12 14:39:32 -0700

    update delete Added

---
+++ src/main/java/com/alireza/mvc/controllers/BooksApi.java
@@ -0,0 +1,51 @@
+package com.alireza.mvc.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alireza.mvc.models.Book;
+import com.alireza.mvc.services.BookService;
+
+@RestController
+public class BooksApi {
+	private final BookService bookService;
+	
+	public BooksApi(BookService bookService) {
+		this.bookService = bookService;
+	}
+	//show all
+	@RequestMapping("/api/books")
+	public List<Book> index(){
+		return bookService.allBooks();
+	}
+	
+    //creat
+	@RequestMapping(value="/api/books", method=RequestMethod.POST)
+    public Book create(@RequestParam(value="title") String title, @RequestParam(value="description") String desc, @RequestParam(value="language") String lang, @RequestParam(value="pages") Integer numOfPages) {
+        Book book = new Book(title, desc, lang, numOfPages);
+        return bookService.createBook(book);
+    }
+	 // other methods removed for brevity updates
+	
+	 @RequestMapping(value="/api/books/{id}", method=RequestMethod.PUT)
+	 public Book update(@PathVariable("id") Long id, @RequestParam(value="title") String title, @RequestParam(value="description") String desc, @RequestParam(value="language") String lang, @RequestParam(value="pages") Integer numOfPages) {
+	    Book book = bookService.updateBook(id, title, desc, lang, numOfPages);
+	    return book;
+	 }
+	//find
+	 @RequestMapping("/api/books/{id}")
+	 public Book show(@PathVariable("id") Long id) {
+	    Book book = bookService.findBook(id);
+	    return book;
+	 } 
+	 //delete 
+	 @RequestMapping(value="/api/books/{id}", method=RequestMethod.DELETE)
+	 public void destroy(@PathVariable("id") Long id){
+		bookService.deleteBook(id);		
+	 }	    
+}
---
+++ src/main/java/com/alireza/mvc/services/BookService.java
@@ -0,0 +1,49 @@
+package com.alireza.mvc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.alireza.mvc.models.Book;
+import com.alireza.mvc.repositories.BookRepository;
+
+@Service 
+public class BookService {
+	private final BookRepository bookRepository;
+	
+	public BookService(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
+	}
+	// returns all the bookscopy
+    public List<Book> allBooks() {
+        return bookRepository.findAll();
+    }
+ // creates a book
+    public Book createBook(Book b) {
+        return bookRepository.save(b);
+    }
+    // retrieves a book
+    public Book findBook(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if(optionalBook.isPresent()) {
+            return optionalBook.get();
+        } else {
+            return null;
+        }
+    }
+    // update 
+	public Book updateBook(Long id, String title, String desc, String lang, Integer numOfPages) {
+		Book book = bookRepository.findById(id).get();
+		book.setTitle(title);
+		book.setDescription(desc);
+		book.setLanguage(lang);
+		book.setNumberOfPages(numOfPages);
+		return bookRepository.save(book);
+	}    
+  //delete Book
+    public void deleteBook(Long id){
+        bookRepository.deleteById(id);    
+    }
+	
+}
